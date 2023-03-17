2959646c2e57ce6f6971a86838f9020aa00ff587
MaximeWeb <Neyret.maxime@gmail.com>
2023-03-17 12:25:36 +0100

    new fichiers

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Form from '../components/Formlogin'
 import { Link } from 'react-router-dom'
+import NavBar from '../components/Navbar'
 import '../styles/Register.css'
 
 
@@ -9,6 +10,9 @@ const Login = () => {
   return (
     <div>
     <Header/>
+    <div className='navbar'>
+    <NavBar />
+    </div>
     <div className='blocregister'>
    <Link to="/signup"><div className='onglet'>Inscrivez-vous !</div></Link>
    <Link to="/profil"><div className='onglet'>Retour au profil</div></Link>
